2da6a29f98366f1e296d2269023e8bc958e4c9bf
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2020-11-07 22:26:11 +0530

    Num Theory GCD 2

--- Notes/Number Theory/GCD.cpp
+++ Notes/Number Theory/GCD.cpp
@@ -2,7 +2,9 @@
 
     // Basic approach
 
-    int gcd(int a,int b){
+    // O(n)
+
+    int gcd(int a, int b){
         int ans = 0;
         for(int i = 1; i<=min(a,b); i++)
             ans = ((a%i == 0 && b%i == 0) && (i>ans)) ? i : ans;
@@ -14,4 +16,16 @@
         cin>>a>>b;
         cout<<gcd(a, b);
         return 0;
+    }
+
+// 2.)
+
+    // Euclid's algo
+
+    // O(log2(n))
+
+    int gcd(int a, int b){
+        if(b == 0)
+            return a;
+        return gcd(b, a%b);
     }
